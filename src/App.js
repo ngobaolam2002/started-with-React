f0400c1f6a060e5fc8f40813b6aec8f096ea1a69
Ngo Bao Lam <ngobaolam31122002@gmail.com>
2023-02-21 15:20:47 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import { Component } from 'react';
 import Main from './components/MainComponent';
-
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
-
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
-
 
 export default App;
